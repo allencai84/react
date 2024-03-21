41cd325350f75812d6f2b1e56007091562451c55
allencai <allencai84@gmai.com>
2024-03-21 16:28:02 +0800

    add next

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Calculator from './Calculator';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Calculator App</h1>
+      <Calculator />
+    </div>
+  );
+}
---
+++ pages/some.js
@@ -0,0 +1,10 @@
+import Form from './test';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Calculator App</h1>
+      <Form />
+    </div>
+  );
+}
